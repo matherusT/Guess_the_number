4feb64c48f58ae1710472641bfe5faad3217e602
matherusT <mateusz.bytner.com>
2021-03-24 20:22:17 +0100

    Add  some function

--- Zgadnij_numer.py
+++ Zgadnij_numer.py
@@ -7,47 +7,77 @@ from random import randint
 import time
 
 def instrukcja():
-    print('''Komputer losuje numer, a użytkownik wpisuje swoje propozycje
-– komputer ocenia, czy podana wartość jest większa czy mniejsza od wylosowanej.
+    print('''Komputer losuje numer, a użytkownik wpisuje swoje propozycje,
+a komputer ocenia, czy podana wartość jest większa czy mniejsza od wylosowanej.
 Liczby możesz odgadywać do skutku.''')
 
 
-def czy_wygrana(wybor, liczba):
+def czy_wygrana(kom, user):
     if liczba == wybor:
         print('Gratulacje! Trafiles/as te liczbe. Ta liczba to: ', liczba)
         return False
-    elif wybor > liczba:
+    elif kom > user and (user > start - 1 and user < end + 1):
         print('Komputer wybral wieksza liczbe')
         return True
-    elif wybor < liczba:
+    elif kom < liczba and (user > start - 1 and user < end + 1):
         print('Komputer wybral mniejsza liczbe')
         return True
-    else:
-        print('Liczba spoza zakresu')
+    elif user < start:
+        print('Blad! Liczba spoza zakresu! Wybierz wieksza liczbe')
+        return True
+    elif user > end:
+        print('Blad! Liczba spoza zakresu! Wybierz mniejsza liczbe')
         return True
     
+def podsumowanie(proby_gracza, gt1, gt2):
+    print()
+    print('Podsumowanie Twojej gry:')
+    print('Zakres gry: ', gt1, '-', gt2, sep = '')
+    print('Twoja liczba prob wyniosla: ', proby_gracza, sep = '')
+    print()
+
+proby = 0    
 instrukcja()
+print()
 print('Wybierz zakres z jakiej puli komputer ma wylosowac liczbe')
+print()
 start = int(input('Podaj poczatek puli: '))
 end = int(input('Podaj koniec puli: '))
-print('Trwa wybieranie liczby z zakresu: ', start, ' i ', end)
+print('Trwa wybieranie liczby z zakresu ', start, ' i ', end, '...', sep = '')
+print()
 time.sleep(3)
 wybor = randint(start, end)
 nowa = True
 
+
 while nowa:
     liczba = int(input('Podaj swoja liczbe: '))
+    print()
+    proby += 1
     if czy_wygrana(wybor, liczba) == False:
-        print()
-        odp = input('Czy chcesz zagrac jeszcze raz? (Y/N): ')
-        if odp.lower() == 'y':
-            nowa = True
-        elif odp.lower() == 'n':
-            nowa = False
-            break
-        else:
-            print('Ups... Blad')
-            break
+        podsumowanie(proby, start, end) 
+        while True:
+            odp = input('Czy chcesz zagrac jeszcze raz? (Y/N): ')
+            print()
+            if odp.lower() == 'y':
+                nowa = True
+                proby = 0
+                print('Wybierz zakres z jakiej puli komputer ma wylosowac liczbe')
+                print()
+                start = int(input('Podaj poczatek puli: '))
+                end = int(input('Podaj koniec puli: '))
+                print('Trwa wybieranie liczby z zakresu: ', start, ' i ', end)
+                print()
+                time.sleep(3)
+                wybor = randint(start, end)
+                break
+            elif odp.lower() == 'n':
+                print('KONIEC')
+                nowa = False
+                break
+            else:
+                print('Ups... Cos poszlo nie tak, sprobuj jeszcze raz')
+                continue
 
-    
+  
 
