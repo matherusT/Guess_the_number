47992386b037a216abd278eb679819ea1fd8ce61
matherusT <81258701+matherusT@users.noreply.github.com>
2022-02-05 20:09:13 +0100

    Update Zgadnij_numer.py

--- Zgadnij_numer.py
+++ Zgadnij_numer.py
@@ -6,7 +6,8 @@ import random
 from random import randint
 import time
 
-def panel():
+
+def dashboard():
     print('''1 - [G]raj
 2 - [Z]asady gry
 3 - [R]ekordy
@@ -15,29 +16,21 @@ def panel():
 6 - [K]oniec
 ''')
     
-def instrukcja():
+    
+def instruction():
     print('''Komputer losuje numer, a użytkownik wpisuje swoje propozycje,
 komputer ocenia, czy podana wartość jest większa czy mniejsza od wylosowanej.
 Liczby możesz odgadywać do skutku.''')
 
-def panel():
-    
-    print('''1 - [G]raj
-2 - [Z]asady gry
-3 - [R]ekordy
-4 - [U]stawienia
-5 - [P]omoc
 
-''')
-    
-def czy_wygrana(kom, user):
-    if liczba == wybor:
-        print('Gratulacje! Trafiles/as te liczbe. Ta liczba to: ', liczba)
+def win(com, user):
+    if number == choice:
+        print('Gratulacje! Trafiles/as te liczbe. Ta liczba to: ', number)
         return False
-    elif kom > user and (user > start - 1 and user < end + 1):
+    elif com > user and (user > start - 1 and user < end + 1):
         print('Komputer wybral wieksza liczbe')
         return True
-    elif kom < liczba and (user > start - 1 and user < end + 1):
+    elif com < number and (user > start - 1 and user < end + 1):
         print('Komputer wybral mniejsza liczbe')
         return True
     elif user < start:
@@ -47,17 +40,19 @@ def czy_wygrana(kom, user):
         print('Blad! Liczba spoza zakresu! Wybierz mniejsza liczbe')
         return True
     
-def podsumowanie(proby_gracza, gt1, gt2):
+    
+def summary(player_att, gt1, gt2):
     print()
     print('Podsumowanie Twojej gry:')
     print('Zakres gry: ', gt1, '-', gt2, sep = '')
-    print('Twoja liczba prob wyniosla: ', proby_gracza, sep = '')
+    print('Twoja liczba prob wyniosla: ', player_att, sep = '')
     print()
 
-def gra():
+    
+def game():
     global start
     global end
-    proby = 0    
+    attempts = 0    
     print()
     print('Wybierz zakres z jakiej puli komputer ma wylosowac liczbe')
     print()
@@ -66,25 +61,26 @@ def gra():
     print('Trwa wybieranie liczby z zakresu ', start, ' i ', end, '...', sep = '')
     print()
     time.sleep(3)
-    global wybor
-    wybor = randint(start, end)
+    global choice
+    choice = randint(start, end)
     
-    nowa = True
+    
+    new = True
 
 
-    while nowa:
-        global liczba
-        liczba = int(input('Podaj swoja liczbe: '))
+    while new:
+        global number
+        number = int(input('Podaj swoja liczbe: '))
         print()
-        proby += 1
-        if czy_wygrana(wybor, liczba) == False:
-            podsumowanie(proby, start, end) 
+        attempts += 1
+        if win(choice, number) == False:
+            summary(attempts, start, end) 
             while True:
-                odp = input('Czy chcesz zagrac jeszcze raz? (Y/N): ')
+                answer = input('Czy chcesz zagrac jeszcze raz? (Y/N): ')
                 print()
-                if odp.lower() == 'y':
-                    nowa = True
-                    proby = 0
+                if answer.lower() == 'y':
+                    new = True
+                    attempts = 0
                     print('Wybierz zakres z jakiej puli komputer ma wylosowac liczbe')
                     print()
                     start = int(input('Podaj poczatek puli: '))
@@ -92,26 +88,27 @@ def gra():
                     print('Trwa wybieranie liczby z zakresu: ', start, ' i ', end)
                     print()
                     time.sleep(3)
-                    wybor = randint(start, end)
+                    choice = randint(start, end)
                     break
-                elif odp.lower() == 'n':
+                elif answer.lower() == 'n':
                     print('KONIEC')
-                    nowa = False
+                    new = False
                     break
                 else:
                     print('Ups... Cos poszlo nie tak, sprobuj jeszcze raz')
                     continue
 
+                    
 while True:
-    panel()
+    dashboard()
     menu = input('Co chcesz zrobic? ')
     print()
     if menu.lower() == 'g':
-        gra()
+        game()
         print()
         continue
     elif menu.lower() == 'z':
-        instrukcja()
+        instruction()
         print()
         continue
     elif menu.lower() == 'r':
